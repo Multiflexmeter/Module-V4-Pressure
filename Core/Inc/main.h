4cd07f415e0f4f99ff5c3804ccef0db1c4e7ce1f
Danny Kerstens <Danny.kerstens@dekimo.com>
2023-09-21 16:16:24 +0200

    Change: cubemx file generation

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -63,10 +63,6 @@ void Error_Handler(void);
 #define MCO_GPIO_Port GPIOH
 #define USART_TX_Enable_Pin GPIO_PIN_1
 #define USART_TX_Enable_GPIO_Port GPIOA
-#define USART_TX_Pin GPIO_PIN_2
-#define USART_TX_GPIO_Port GPIOA
-#define USART_RX_Pin GPIO_PIN_3
-#define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
 #define USART_RX_Enable_Pin GPIO_PIN_12
