35327e6a1c36835c04132d1c8a679bc03d853581
Danny Kerstens <Danny.kerstens@dekimo.com>
2023-10-20 10:42:44 +0200

    Change: generated files based on changing the interrupt priorities

--- Core/Src/stm32l0xx_hal_msp.c
+++ Core/Src/stm32l0xx_hal_msp.c
@@ -191,7 +191,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
     /* Peripheral clock enable */
     __HAL_RCC_I2C1_CLK_ENABLE();
     /* I2C1 interrupt Init */
-    HAL_NVIC_SetPriority(I2C1_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(I2C1_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(I2C1_IRQn);
   /* USER CODE BEGIN I2C1_MspInit 1 */
 
