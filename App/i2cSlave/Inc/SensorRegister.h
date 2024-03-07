ff8b9a5bf53fd681db8a8541e7b397b90241e595
Peter Kwekkeboom <peter.kwekkeboom@dekimo.com>
2024-03-07 13:04:56 +0100

    change: software version to 0.5

--- App/i2cSlave/Inc/SensorRegister.h
+++ App/i2cSlave/Inc/SensorRegister.h
@@ -21,7 +21,7 @@
 #define REG_ERROR_STATUS          0x51
 
 /* Registers Defaults*/
-#define DEF_FIRMWARE_VERSION  "0.5c"
+#define DEF_FIRMWARE_VERSION  "0.5"
 #define DEF_PROTOCOL_VERSION  0x00
 #define DEF_SENSOR_TYPE       0x01
 #define DEF_INIT_START        0x00
